b9c03e72d0fc106442e7b2f5ade44ff3202371b0
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-11-20 14:41:56 +0000

    Ch3-Ex1
    Exercise 1 from Chapter 3 of the Codelab activities.
    This code does not fully work yet and does not contain comments.
    More commits to follow.

---
+++ Ch3-Ex1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int forbiddenNumber = 5;
+	int userInput = 0;
+	while (userInput != forbiddenNumber) {
+		cout << "Enter a number that isn't 5" << endl;
+		cin >> userInput;
+		if (userInput == forbiddenNumber) {
+			break;
+			cout << "Hey! You weren't supposed to enter 5!" << endl;
+		}
+		else {
+			continue;
+		}
+	}
+}
