17f2eff4e7d02facc9ab904735cd6437e1220772
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-11-20 14:59:54 +0000

    Update Ch3-Ex1.cpp
    Added comments.
    This should be the last commit unless some giant bug is found.

--- Ch3-Ex1.cpp
+++ Ch3-Ex1.cpp
@@ -3,19 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-int main()
+int main() // Starts main function.
 {
 	int forbiddenNumber = 5;
 	int userInput = 0;
+	// Initialises The Forbidden Number and the user input.
+	// These will be used later.
 	while (userInput != forbiddenNumber) {
 		cout << "Enter a number that isn't 5" << endl;
 		cin >> userInput;
+		// Create a while loop that executes whilst the user
+		// input and The Forbidden Number are not equal.
+		// Ask the user to input any other number than 5.
 		if (userInput == forbiddenNumber) {
 			cout << "Hey! You weren't supposed to enter 5!" << endl;
 			break;
+			// You fool. You entered The Forbidden Number.
 		}
 		else {
 			continue;
+			// Continues the loop if 5 is not entered.
 		}
 	}
-}
+	return(0);
+}          // Ends main function.
